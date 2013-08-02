a20f6776691d8b84cb44bbfcc36dd56cfd792ee4
Max Peterson <max@incuna.com>
2013-08-02 10:17:42 +0100

    Usage tweaks.

--- makeTextFiles.js
+++ makeTextFiles.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict';
+
 // Nodejs libs.
 var fs = require('fs');
 var path = require('path');
@@ -7,16 +9,44 @@ var requirejs = require('requirejs');
 var glob = require("glob");
 var _ = require('lodash');
 
-var arguments = process.argv.splice(2);
+var program = process.argv[1];
+var args = process.argv.splice(2);
 
-var proj_dir = path.resolve(arguments.length ? arguments.shift() : '.');
-var templatePath = path.join(__dirname, 'textFiles.template.js');
+function Usage(ret) {
+    console.log('Usage');
+    console.log(program+' [path/to/settings.js]');
+    process.exit(ret||0);
+}
+
+var settingsPath;
+if (args.length) {
+    // settingsPath specified
+    settingsPath = args.shift();
+} else {
+    // Guess settingsPath
+    var cwd = process.cwd();
+    settingsPath = path.join(cwd, 'settings.js');
+    if (!fs.existsSync(settingsPath)) {
+        var found = glob.sync("*/settings.js");
+        if (found.length) {
+            settingsPath = path.join(cwd, found[0]);
+        }
+    }
+    console.log('Using '+settingsPath);
+}
+
+if (!fs.existsSync(settingsPath)) {
+    console.log('Cant find settings.js');
+    Usage(1);
+}
+
+var proj_dir = path.dirname(settingsPath);
 var destinationPath = path.join(proj_dir, 'textFiles.js');
-var settingsPath = path.join(proj_dir, 'settings.js');
+var require_config = path.join(proj_dir, 'jam', 'require.config.js');
+var templatePath = path.join(__dirname, 'textFiles.template.js');
 
 
 // Use require to resolve module paths.
-var require_config = path.join(proj_dir, 'jam', 'require.config.js');
 if (fs.existsSync(require_config)) {
     requirejs(require_config);
 } else {
@@ -55,12 +85,18 @@ fs.readFile(templatePath, 'utf8', function (err, template) {
         return _(files).map(function (file) {return path.join(module, file);});
     }).flatten().value();
 
+    if (!filePaths) {
+        console.log('No files found');
+        process.exit(2);
+    }
+
     var sortedPaths = filePaths.sort();
     var textPaths = _(sortedPaths).map(function (filePath) {
         return 'text!' + filePath
     });
 
     // Write the textFiles
+    console.log('Writting '+destinationPath);
     var fileTemplate = _.template(template);
     fs.writeFile(destinationPath, fileTemplate({
         textFiles: sortedPaths.join("',\n        '"),
